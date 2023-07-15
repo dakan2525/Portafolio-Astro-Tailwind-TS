a1dfa39444558fc334718a13f721b4ae39b70e61
Dakan <andres25prado@gmail.com>
2023-07-15 18:09:28 -0500

    prueba publicación

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  adapter: cloudflare(),
+  output: 'hybrid'
 });
