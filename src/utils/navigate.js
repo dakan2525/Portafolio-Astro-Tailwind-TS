a835c5951552ca81c3d59d7659e108606266a0f8
Dakan <andres25prado@gmail.com>
2023-07-19 09:54:36 -0500

    error vercel3

--- src/utils/navigate.js
+++ src/utils/navigate.js
@@ -1,36 +1,30 @@
 const checkSupported = () => {
-    return !!document.startViewTransition; // Retorna true si document.startViewTransition es una función, y false si es null, undefined, o cualquier otro valor falsy.
+    return Boolean(document.startViewTransition)
 }
 
-export default function Navigate () {
-
-if (!checkSupported()) return
-
-    
-    window.navigation.addEventListener('navigate', (event) => {
-        const toUrl = new URL(event.destination.url);
+const navigate = (event) => {
+    const toUrl = new URL(event.destination.url);
 
     if (location.origin !== toUrl.origin) return;
-    // es una página externa? si es así, lo ignoramos
-        
-    // si es una navegación en el mismo dominio (origen)
+
     event.intercept({
-        async handler () {
-            // vamos a cargar la página de destino
-            // utilizando un fetch para obtener el HTML
-            const response = await fetch(toUrl.pathname); // /clean-code
+        async handler() {
+            const response = await fetch(toUrl.pathname);
             const text = await response.text();
-            // quedarnos sólo con el contenido del html dentro de la etiqueta body
-            // usamos un regex para extraerlo
             const data = text.match(/<body[^>]*>([\s\S]*)<\/body>/i)[1];
-    
-            // utilizar la api de View Transition API
+
             document.startViewTransition(() => {
-                // el scroll hacia arriba del todo
                 document.body.innerHTML = data;
                 document.documentElement.scrollTop = 1;
-            })
-        }
-    })
-})
-}
+            });
+        },
+    });
+};
+
+const initializeNavigation = () => {
+    if (!checkSupported()) return;
+
+    window.navigation.addEventListener('navigate', navigate);
+};
+
+export default initializeNavigation;
