46dbf425608b5c7c94655406332bec10fcb3e574
Dakan <andres25prado@gmail.com>
2023-07-19 08:53:35 -0500

    add proyectos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import preact from "@astrojs/preact";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [preact(), tailwind()],
   adapter: cloudflare(),
   output: 'hybrid'
 });
