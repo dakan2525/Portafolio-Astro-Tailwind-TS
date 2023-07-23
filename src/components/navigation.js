72739ab6b2536f6baf80a648d0aa01bd16c107ef
Dakan <andres25prado@gmail.com>
2023-07-23 18:43:39 -0500

    git commit -m

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,3 +1,4 @@
+//Intercepta las transiciones de navegación
 if (document.startViewTransition) {
     window.navigation.addEventListener('navigate', async (event) => {
         const toUrl = new URL(event.destination.url);
@@ -23,8 +24,8 @@ if (document.startViewTransition) {
 
                     // utilizar la API de View Transition
                     document.startViewTransition(() => {
-                        // el scroll hacia arriba del todo
                         document.body.innerHTML = mainContent;
+                        // el scroll hacia arriba del todo
                         // document.documentElement.scrollTop = 1;
                     });
                 } catch (error) {
@@ -51,14 +52,8 @@ if (document.startViewTransition) {
 
 
 
-
-
-
-
-
-
 // if (document.startViewTransition) {
-//     window.navigation.addEventListener('navigate', (event) => {
+//     window.navigation.addEventListener('navigate', async (event) => {
 //         const toUrl = new URL(event.destination.url);
 
 //         // es una página externa? si es así, lo ignoramos
@@ -67,23 +62,28 @@ if (document.startViewTransition) {
 //         // si es una navegación en el mismo dominio (origen)
 //         event.intercept({
 //             async handler() {
-//                 // vamos a cargar la página de destino
-//                 // utilizando un fetch para obtener el HTML
-//                 const response = await fetch(toUrl.pathname);
-//                 const text = await response.text();
-//                 // quedarnos sólo con el contenido del html dentro de la etiqueta body
-//                 // usamos un regex para extraerlo
-//                 // const data = text.match(/<body[^>]*>([\s\S]*)<\/body>/i)[1];
+//                 try {
+//                     // vamos a cargar la página de destino
+//                     // utilizando un fetch para obtener el HTML
+//                     const response = await fetch(toUrl.pathname);
+//                     const text = await response.text();
 
-//                 const bodyMatch = text.match(/<main[^>]*>([\s\S]*)<\/main>/i);
-//                 const data = bodyMatch ? bodyMatch[1] : '';
+//                     // Crear un nuevo elemento div y establecer el HTML cargado en él
+//                     const tempDiv = document.createElement('div');
+//                     tempDiv.innerHTML = text;
 
-//                 // utilizar la api de View Transition API
-//                 document.startViewTransition(() => {
-//                     // el scroll hacia arriba del todo
-//                     document.body.innerHTML = data;
-//                     document.documentElement.scrollTop = 1;
-//                 });
+//                     // Obtener el contenido dentro de la etiqueta <main>
+//                     const mainContent = tempDiv.querySelector('main').innerHTML;
+
+//                     // utilizar la API de View Transition
+//                     document.startViewTransition(() => {
+//                         document.body.innerHTML = mainContent;
+//                         // el scroll hacia arriba del todo
+//                         document.documentElement.scrollTop = 1;
+//                     });
+//                 } catch (error) {
+//                     console.error('Error al cargar la página:', error);
+//                 }
 //             },
 //         });
 //     });
